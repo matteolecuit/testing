8663af18f53612f1ce490b28889fa13828e6282a
Matteo Lecuit <matteo@lecuit.fr>
2022-03-25 11:27:07 +0100

    fix package.json

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -4,7 +4,7 @@ import * as creditCardService from "../creditCardService";
 beforeAll(() => {
   jest
     .spyOn(creditCardService, "initCreditCardPayment")
-    .mockImplementation(() => Promise.resolve());
+    .mockImplementation(() => Promise.resolve(true));
 });
 test("should send creditCardPayment request if moneyInCoinAcceptor = 0", async () => {
   const moneyInCoinAcceptor = 0;
--- src/creditCardService.ts
+++ src/creditCardService.ts
@@ -1,3 +1,3 @@
 export const initCreditCardPayment = async () => {
-  return;
+  return false;
 };
